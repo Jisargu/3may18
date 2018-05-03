d217e2b64b6b7c41445a51ab4470db8b0957e579
Jisargu <jonathnsaa@gmail.com>
2018-05-03 11:30:28 -0500

    Ejercicios JS.  Hardcodear la tabla del 1, 2 y 3 usando JS

---
+++ js/ejercicio1.js
@@ -0,0 +1,44 @@
+function codigo_html(){
+    var ID_A_MODIFICAR='tabla';
+    var texto= '<div class="row">\
+                    <div class="col-2">Tabla del 1</div>\
+                    <div class="col-1">1</div>\
+                    <div class="col-1">2</div>\
+                    <div class="col-1">3</div>\
+                    <div class="col-1">4</div>\
+                    <div class="col-1">5</div>\
+                    <div class="col-1">6</div>\
+                    <div class="col-1">7</div>\
+                    <div class="col-1">8</div>\
+                    <div class="col-1">9</div>\
+                    <div class="col-1">10</div>\
+                </div>\
+                <div class="row">\
+                    <div class="col-2">Tabla del 2</div>\
+                    <div class="col-1">2</div>\
+                    <div class="col-1">4</div>\
+                    <div class="col-1">6</div>\
+                    <div class="col-1">8</div>\
+                    <div class="col-1">10</div>\
+                    <div class="col-1">12</div>\
+                    <div class="col-1">14</div>\
+                    <div class="col-1">16</div>\
+                    <div class="col-1">18</div>\
+                    <div class="col-1">20</div>\
+                </div>\
+                <div class="row">\
+                    <div class="col-2">Tabla del 3</div>\
+                    <div class="col-1">3</div>\
+                    <div class="col-1">6</div>\
+                    <div class="col-1">9</div>\
+                    <div class="col-1">12</div>\
+                    <div class="col-1">15</div>\
+                    <div class="col-1">18</div>\
+                    <div class="col-1">21</div>\
+                    <div class="col-1">24</div>\
+                    <div class="col-1">27</div>\
+                    <div class="col-1">30</div>\
+                </div>\
+                ';
+    document.getElementById(ID_A_MODIFICAR).innerHTML = texto;
+}
